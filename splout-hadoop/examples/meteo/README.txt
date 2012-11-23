f7d4be9b548dee0af2faf2f0211a25ec8212904f
Iván de Prado <ivan.prado@gmail.com>
2012-11-23 13:33:42 +0100

    finalStatements and preInsertStatements on JSON

--- splout-hadoop/examples/meteo/README.txt
+++ splout-hadoop/examples/meteo/README.txt
@@ -16,31 +16,7 @@ Generate meteo table:
 
 --root database-files --tablespaces meteo_table --replication 2 --qnode http://localhost:4412
 
-0,5,7,11,14,17,18,19,20,21,14,21,24,29,31,32,35,40,42,43,46,51,53,54,57,62,64,65,68,72,74,75,78,82,84,85,88,92,95,99,102,107,108,108,110,115,116,116,118,122,123,123,125,129,132,137
-
-STN---  1-6       Int.   Station number (WMO/DATSAV3 number)
-WBAN    8-12      Int.   WBAN number where applicable--this is the
-YEAR    15-18     Int.   The year.
-MODA    19-22     Int.   The month and day.
-TEMP    25-30     Real   Mean temperature for the day in degrees
-Count   32-33     Int.   Number of observations used in 
-DEWP    36-41     Real   Mean dew point for the day in degrees
-Count   43-44     Int.   Number of observations used in 
-SLP     47-52     Real   Mean sea level pressure for the day
-Count   54-55     Int.   Number of observations used in 
-STP     58-63     Real   Mean station pressure for the day
-Count   65-66     Int.   Number of observations used in 
-VISIB   69-73     Real   Mean visibility for the day in miles
-Count   75-76     Int.   Number of observations used in 
-WDSP    79-83     Real   Mean wind speed for the day in knots
-Count   85-86     Int.   Number of observations used in 
-MXSPD   89-93     Real   Maximum sustained wind speed reported 
-GUST    96-100    Real   Maximum wind gust reported for the day
-MAX     103-108   Real   Maximum temperature reported during the 
-Flag    109-109   Char   Blank indicates max temp was taken from the
-MIN     111-116   Real   Minimum temperature reported during the 
-Flag    117-117   Char   Blank indicates min temp was taken from the
-PRCP    119-123   Real   Total precipitation (rain and/or melted
-Flag    124-124   Char   A = 1 report of 6-hour precipitation 
-SNDP    126-130   Real   Snow depth in inches to tenths--last     
-FRSHTT  133-138   Int.   Indicators (1 = yes, 0 = no/not          
+Meteo from definition file:
+---------------------------
+GeneratorCMD -tf examples/meteo/meteo-pby-stn-wban.json -tf examples/meteo/meteo-pby-date.json -o database-files
+DeployerCMD --qnode http://localhost:4412 --root database-files --tablespaces meteo-pby-stn-wban
--- splout-hadoop/src/main/java/com/splout/db/hadoop/JSONTablespaceDefinition.java
+++ splout-hadoop/src/main/java/com/splout/db/hadoop/JSONTablespaceDefinition.java
@@ -20,271 +20,308 @@ package com.splout.db.hadoop;
  * #L%
  */
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.hadoop.fs.Path;
-
 import com.datasalt.pangool.io.Fields;
 import com.datasalt.pangool.io.Schema;
 import com.datasalt.pangool.tuplemr.mapred.lib.input.TupleTextInputFormat;
 import com.splout.db.hadoop.TableBuilder.TableBuilderException;
 import com.splout.db.hadoop.TablespaceBuilder.TablespaceBuilderException;
+import org.apache.hadoop.fs.Path;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A JSON-friendly bean that can map to a {@link TablespaceSpec} easily. It only supports text input. Use
  * {@link #build()} for obtaining a TablespaceSpec instance.
- * <p>
+ * <p/>
  * For specifying compound indexes use "," separator in the string index definition.
  */
 public class JSONTablespaceDefinition {
 
-	private String name;
-	private int nPartitions;
-	private List<JSONTableDefinition> partitionedTables;
-	private List<JSONTableDefinition> replicateAllTables = new ArrayList<JSONTableDefinition>();
+  private String name;
+  private int nPartitions;
+  private List<JSONTableDefinition> partitionedTables;
+  private List<JSONTableDefinition> replicateAllTables = new ArrayList<JSONTableDefinition>();
 
-	/*
-	 * Inner static method for converting a {@link JSONTableDefinition} into a {@link Table} bean through {@link TableBuilder}.
-	 */
-	private static Table buildTable(JSONTableDefinition table, boolean isReplicateAll) throws TableBuilderException {
-		if(table.getName() == null) {
-			throw new IllegalArgumentException("Must provide a name for all tables.");
-		}
-		if(table.getSchema() == null) {
-			throw new IllegalArgumentException("Must provide an schema for all tables.");
-		}
-		if(!isReplicateAll && (table.getPartitionFields() == null || table.getPartitionFields() == null || table.getPartitionFields().length() == 0)) {
-			throw new IllegalArgumentException("Partitioned table must be partitioned by some field.");			
-		}
-		if(table.getTableInputs() == null || table.getTableInputs().size() == 0) {
-			throw new IllegalArgumentException("Table must have some table inputs.");			
-		}
-		
-		Schema schema = new Schema(table.getName(), Fields.parse(table.getSchema()));
-		TableBuilder tableBuilder = new TableBuilder(schema);
-		for(String index : table.getIndexes()) {
-			tableBuilder.createIndex(index.split(","));
-		}
-		
-		if(!isReplicateAll) {
-			tableBuilder.partitionBy(table.getPartitionFields().split(","));
-		}
-		
-		for(JSONTableInputDefinition tableInput : table.getTableInputs()) {
-			TextInputSpecs specs = tableInput.getInputSpecs();
-			if(specs == null) {
-				specs = new TextInputSpecs(); // default specs (tabulated file)
-			}
-			if(tableInput.getPaths() == null || tableInput.getPaths().size() == 0) {
-				throw new IllegalArgumentException("All table inputs must have input paths.");
-			}
-			
-			for(String file : tableInput.getPaths()) {
-				if (specs.getFixedWidthFields() != null) {
-					// Fixed width fields definition.
-				  int[] fieldsArr = new int[specs.getFixedWidthFields().size()];
-				  for(int i = 0;i < fieldsArr.length;i++) {
-				    fieldsArr[i] = specs.getFixedWidthFields().get(i);
-				  }
-					tableBuilder.addFixedWidthTextFile(new Path(file), schema, fieldsArr, specs.isSkipHeader(), specs.getNullString(), null);
-				} else {
-					// CSV definition
-					tableBuilder.addCSVTextFile(file, specs.getSeparatorChar(), specs.getQuotesChar(), specs.getEscapeChar(),
-					    specs.isSkipHeader(), specs.isStrictQuotes(), specs.getNullString());
-					
-				}
-			}
-		}
-		
-		if(isReplicateAll) {
-			tableBuilder.replicateToAll();
-		}
-		return tableBuilder.build();
-	}
-	
-	/**
-	 * Use this method for obtaining a {@link TablespaceSpec} through {@link TablespaceBuilder}.
-	 */
-	public TablespaceSpec build() throws TablespaceBuilderException, TableBuilderException {
-		TablespaceBuilder builder = new TablespaceBuilder();
-		builder.setNPartitions(nPartitions);
-
-		if(partitionedTables == null) {
-			throw new IllegalArgumentException("Can't build a " + TablespaceSpec.class.getName()
-			    + " without any partitioned table.");
-		}
-		
-		if(name == null) {
-			throw new IllegalArgumentException("Must provide a name for the Tablespace.");			
-		}
-
-		for(JSONTableDefinition table : partitionedTables) {
-			builder.add(buildTable(table, false));
-		}
-		
-		for(JSONTableDefinition table : replicateAllTables) {
-			builder.add(buildTable(table, true));
-		}
-
-		return builder.build();
-	}
-
-	public static class JSONTableDefinition {
-
-		private String name;
-		private List<JSONTableInputDefinition> tableInputs;
-		private String schema;
-		private String partitionFields;
-		private List<String> indexes = new ArrayList<String>();
-
-		public List<JSONTableInputDefinition> getTableInputs() {
-			return tableInputs;
-		}
-		public void setTableInputs(List<JSONTableInputDefinition> tableInputs) {
-			this.tableInputs = tableInputs;
-		}
-		public String getSchema() {
-			return schema;
-		}
-		public void setSchema(String schema) {
-			this.schema = schema;
-		}
-		public String getPartitionFields() {
-			return partitionFields;
-		}
-		public void setPartitionFields(String partitionFields) {
-			this.partitionFields = partitionFields;
-		}
-		public List<String> getIndexes() {
-			return indexes;
-		}
-		public void setIndexes(List<String> indexes) {
-			this.indexes = indexes;
-		}
-		public String getName() {
-    	return name;
+  /*
+  * Inner static method for converting a {@link JSONTableDefinition} into a {@link Table} bean through {@link TableBuilder}.
+  */
+  private static Table buildTable(JSONTableDefinition table, boolean isReplicateAll) throws TableBuilderException {
+    if (table.getName() == null) {
+      throw new IllegalArgumentException("Must provide a name for all tables.");
     }
-		public void setName(String name) {
-    	this.name = name;
+    if (table.getSchema() == null) {
+      throw new IllegalArgumentException("Must provide an schema for all tables.");
     }
-	}
-
-	public static class JSONTableInputDefinition {
-
-		private TextInputSpecs inputSpecs;
-		private List<String> paths;
-
-		public TextInputSpecs getInputSpecs() {
-			return inputSpecs;
-		}
-		public void setInputSpecs(TextInputSpecs inputSpecs) {
-			this.inputSpecs = inputSpecs;
-		}
-		public List<String> getPaths() {
-    	return paths;
+    if (!isReplicateAll && (table.getPartitionFields() == null || table.getPartitionFields() == null || table.getPartitionFields().length() == 0)) {
+      throw new IllegalArgumentException("Partitioned table must be partitioned by some field.");
     }
-		public void setPaths(List<String> paths) {
-    	this.paths = paths;
-    }
-	}
-
-	public static class TextInputSpecs {
-
-		private char separatorChar = '\t';
-		private char quotesChar = TupleTextInputFormat.NO_QUOTE_CHARACTER;
-		private char escapeChar = TupleTextInputFormat.NO_ESCAPE_CHARACTER;
-		private boolean skipHeader = false;
-		private boolean strictQuotes = false;
-		private String nullString = TupleTextInputFormat.NO_NULL_STRING;
-		private ArrayList<Integer>  fixedWidthFields = null; 
-
-		public char getSeparatorChar() {
-			return separatorChar;
-		}
-
-		public void setSeparatorChar(char separatorChar) {
-			this.separatorChar = separatorChar;
-		}
-
-		public char getQuotesChar() {
-			return quotesChar;
-		}
-
-		public void setQuotesChar(char quotesChar) {
-			this.quotesChar = quotesChar;
-		}
-
-		public char getEscapeChar() {
-			return escapeChar;
-		}
-
-		public void setEscapeChar(char escapeChar) {
-			this.escapeChar = escapeChar;
-		}
-
-		public boolean isSkipHeader() {
-			return skipHeader;
-		}
-
-		public void setSkipHeader(boolean skipHeader) {
-			this.skipHeader = skipHeader;
-		}
-
-		public boolean isStrictQuotes() {
-    	return strictQuotes;
+    if (table.getTableInputs() == null || table.getTableInputs().size() == 0) {
+      throw new IllegalArgumentException("Table must have some table inputs.");
     }
 
-		public void setStrictQuotes(boolean strictQuotes) {
-    	this.strictQuotes = strictQuotes;
+    Schema schema = new Schema(table.getName(), Fields.parse(table.getSchema()));
+    TableBuilder tableBuilder = new TableBuilder(schema);
+    for (String index : table.getIndexes()) {
+      tableBuilder.createIndex(index.split(","));
     }
 
-		public String getNullString() {
-    	return nullString;
+    if (!isReplicateAll) {
+      tableBuilder.partitionBy(table.getPartitionFields().split(","));
     }
 
-		public void setNullString(String nullString) {
-    	this.nullString = nullString;
+    // Adding pre and post SQL
+    if (table.getPreInsertStatements().size() != 0) {
+      tableBuilder.preSQL(table.getPreInsertStatements().toArray(new String[0]));
+    }
+    if (table.getFinalStatements().size() != 0) {
+      tableBuilder.postSQL(table.getFinalStatements().toArray(new String[0]));
     }
 
-		public ArrayList<Integer> getFixedWidthFields() {
-			return fixedWidthFields;
-		}
+    for (JSONTableInputDefinition tableInput : table.getTableInputs()) {
+      TextInputSpecs specs = tableInput.getInputSpecs();
+      if (specs == null) {
+        specs = new TextInputSpecs(); // default specs (tabulated file)
+      }
+      if (tableInput.getPaths() == null || tableInput.getPaths().size() == 0) {
+        throw new IllegalArgumentException("All table inputs must have input paths.");
+      }
 
-		public void setFixedWidthFields(ArrayList<Integer> fixedWidthFields) {
-			this.fixedWidthFields = fixedWidthFields;
-		}
-	}
+      for (String file : tableInput.getPaths()) {
+        if (specs.getFixedWidthFields() != null) {
+          // Fixed width fields definition.
+          int[] fieldsArr = new int[specs.getFixedWidthFields().size()];
+          for (int i = 0; i < fieldsArr.length; i++) {
+            fieldsArr[i] = specs.getFixedWidthFields().get(i);
+          }
+          tableBuilder.addFixedWidthTextFile(new Path(file), schema, fieldsArr, specs.isSkipHeader(), specs.getNullString(), null);
+        } else {
+          // CSV definition
+          tableBuilder.addCSVTextFile(file, specs.getSeparatorChar(), specs.getQuotesChar(), specs.getEscapeChar(),
+              specs.isSkipHeader(), specs.isStrictQuotes(), specs.getNullString());
 
-	public int getnPartitions() {
-		return nPartitions;
-	}
+        }
+      }
+    }
 
-	public void setnPartitions(int nPartitions) {
-		this.nPartitions = nPartitions;
-	}
+    if (isReplicateAll) {
+      tableBuilder.replicateToAll();
+    }
+    return tableBuilder.build();
+  }
 
-	public List<JSONTableDefinition> getPartitionedTables() {
-		return partitionedTables;
-	}
+  /**
+   * Use this method for obtaining a {@link TablespaceSpec} through {@link TablespaceBuilder}.
+   */
+  public TablespaceSpec build() throws TablespaceBuilderException, TableBuilderException {
+    TablespaceBuilder builder = new TablespaceBuilder();
+    builder.setNPartitions(nPartitions);
 
-	public void setPartitionedTables(List<JSONTableDefinition> partitionedTables) {
-		this.partitionedTables = partitionedTables;
-	}
+    if (partitionedTables == null) {
+      throw new IllegalArgumentException("Can't build a " + TablespaceSpec.class.getName()
+          + " without any partitioned table.");
+    }
 
-	public List<JSONTableDefinition> getReplicateAllTables() {
-		return replicateAllTables;
-	}
+    if (name == null) {
+      throw new IllegalArgumentException("Must provide a name for the Tablespace.");
+    }
 
-	public void setReplicateAllTables(List<JSONTableDefinition> replicateAllTables) {
-		this.replicateAllTables = replicateAllTables;
-	}
+    for (JSONTableDefinition table : partitionedTables) {
+      builder.add(buildTable(table, false));
+    }
 
-	public String getName() {
-		return name;
-	}
+    for (JSONTableDefinition table : replicateAllTables) {
+      builder.add(buildTable(table, true));
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    return builder.build();
+  }
+
+  public static class JSONTableDefinition {
+
+    private String name;
+    private List<JSONTableInputDefinition> tableInputs;
+    private String schema;
+    private String partitionFields;
+    private List<String> indexes = new ArrayList<String>();
+    private List<String> preInsertStatements = new ArrayList<String>();
+    private List<String> finalStatements = new ArrayList<String>();
+
+    public List<JSONTableInputDefinition> getTableInputs() {
+      return tableInputs;
+    }
+
+    public void setTableInputs(List<JSONTableInputDefinition> tableInputs) {
+      this.tableInputs = tableInputs;
+    }
+
+    public String getSchema() {
+      return schema;
+    }
+
+    public void setSchema(String schema) {
+      this.schema = schema;
+    }
+
+    public String getPartitionFields() {
+      return partitionFields;
+    }
+
+    public void setPartitionFields(String partitionFields) {
+      this.partitionFields = partitionFields;
+    }
+
+    public List<String> getIndexes() {
+      return indexes;
+    }
+
+    public void setIndexes(List<String> indexes) {
+      this.indexes = indexes;
+    }
+
+    public String getName() {
+      return name;
+    }
+
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public List<String> getPreInsertStatements() {
+      return preInsertStatements;
+    }
+
+    public void setPreInsertStatements(List<String> preInsertStatements) {
+      this.preInsertStatements = preInsertStatements;
+    }
+
+    public List<String> getFinalStatements() {
+      return finalStatements;
+    }
+
+    public void setFinalStatements(List<String> finalStatements) {
+      this.finalStatements = finalStatements;
+    }
+  }
+
+  public static class JSONTableInputDefinition {
+
+    private TextInputSpecs inputSpecs;
+    private List<String> paths;
+
+    public TextInputSpecs getInputSpecs() {
+      return inputSpecs;
+    }
+
+    public void setInputSpecs(TextInputSpecs inputSpecs) {
+      this.inputSpecs = inputSpecs;
+    }
+
+    public List<String> getPaths() {
+      return paths;
+    }
+
+    public void setPaths(List<String> paths) {
+      this.paths = paths;
+    }
+  }
+
+  public static class TextInputSpecs {
+
+    private char separatorChar = '\t';
+    private char quotesChar = TupleTextInputFormat.NO_QUOTE_CHARACTER;
+    private char escapeChar = TupleTextInputFormat.NO_ESCAPE_CHARACTER;
+    private boolean skipHeader = false;
+    private boolean strictQuotes = false;
+    private String nullString = TupleTextInputFormat.NO_NULL_STRING;
+    private ArrayList<Integer> fixedWidthFields = null;
+
+    public char getSeparatorChar() {
+      return separatorChar;
+    }
+
+    public void setSeparatorChar(char separatorChar) {
+      this.separatorChar = separatorChar;
+    }
+
+    public char getQuotesChar() {
+      return quotesChar;
+    }
+
+    public void setQuotesChar(char quotesChar) {
+      this.quotesChar = quotesChar;
+    }
+
+    public char getEscapeChar() {
+      return escapeChar;
+    }
+
+    public void setEscapeChar(char escapeChar) {
+      this.escapeChar = escapeChar;
+    }
+
+    public boolean isSkipHeader() {
+      return skipHeader;
+    }
+
+    public void setSkipHeader(boolean skipHeader) {
+      this.skipHeader = skipHeader;
+    }
+
+    public boolean isStrictQuotes() {
+      return strictQuotes;
+    }
+
+    public void setStrictQuotes(boolean strictQuotes) {
+      this.strictQuotes = strictQuotes;
+    }
+
+    public String getNullString() {
+      return nullString;
+    }
+
+    public void setNullString(String nullString) {
+      this.nullString = nullString;
+    }
+
+    public ArrayList<Integer> getFixedWidthFields() {
+      return fixedWidthFields;
+    }
+
+    public void setFixedWidthFields(ArrayList<Integer> fixedWidthFields) {
+      this.fixedWidthFields = fixedWidthFields;
+    }
+  }
+
+  public int getnPartitions() {
+    return nPartitions;
+  }
+
+  public void setnPartitions(int nPartitions) {
+    this.nPartitions = nPartitions;
+  }
+
+  public List<JSONTableDefinition> getPartitionedTables() {
+    return partitionedTables;
+  }
+
+  public void setPartitionedTables(List<JSONTableDefinition> partitionedTables) {
+    this.partitionedTables = partitionedTables;
+  }
+
+  public List<JSONTableDefinition> getReplicateAllTables() {
+    return replicateAllTables;
+  }
+
+  public void setReplicateAllTables(List<JSONTableDefinition> replicateAllTables) {
+    this.replicateAllTables = replicateAllTables;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
 }
