978cd0ff0bf6245124b162167b800fc1fa8d45d8
Iván de Prado <ivan.prado@gmail.com>
2012-12-04 17:18:00 +0100

    Support in CMD API for initStatements on serving.

--- splout-hadoop/src/main/java/com/splout/db/hadoop/DeployerCMD.java
+++ splout-hadoop/src/main/java/com/splout/db/hadoop/DeployerCMD.java
@@ -20,11 +20,11 @@ package com.splout.db.hadoop;
  * #L%
  */
 
-import java.io.File;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.List;
-
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.google.common.io.Files;
+import com.splout.db.common.JSONSerDe;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
@@ -32,106 +32,109 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.codehaus.jackson.type.TypeReference;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
-import com.beust.jcommander.ParameterException;
-import com.google.common.io.Files;
-import com.splout.db.common.JSONSerDe;
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * This tool can be used to deploy tablespaces once they are generated. 
- * It allows to deploy several tablespaces at a time. 
+ * This tool can be used to deploy tablespaces once they are generated.
+ * It allows to deploy several tablespaces at a time.
  * You can specify here the replication factor and the QNode to deploy to.
  */
 public class DeployerCMD implements Tool {
 
-	@Parameter(names = { "-r", "--replication" }, description = "The replication factor to use for all the tablespace, defaults to 1.")
-	private Integer replicationFactor = 1;
+  @Parameter(names = {"-r", "--replication"}, description = "The replication factor to use for all the tablespace, defaults to 1.")
+  private Integer replicationFactor = 1;
 
-	@Parameter(required = true, names = { "-q", "--qnode" }, description = "A QNode address, will be used for deploying the tablespace.")
-	private String qnode;
+  @Parameter(required = true, names = {"-q", "--qnode"}, description = "A QNode address, will be used for deploying the tablespace.")
+  private String qnode;
 
-	@Parameter(names = { "-root", "--root" }, description = "The path where the generated tablespace or tablespaces are. If you are running the process from Hadoop, relative paths would use the Hadoop filesystem. Use full qualified URIs instead if you want other behaviour.")
-	private String root;
+  @Parameter(names = {"-root", "--root"}, description = "The path where the generated tablespace or tablespaces are. If you are running the process from Hadoop, relative paths would use the Hadoop filesystem. Use full qualified URIs instead if you want other behaviour.")
+  private String root;
 
-	@Parameter(names = { "-tn", "--tablespacename" }, description = "If root is a generated tablespace - for example, a bucket in S3 - this parameter provides its name. Either use this or tablespaces parameter if root contains multiple tablespaces.")
-	private String tablespaceName = null;
-	
-	@Parameter(names = { "-ts", "--tablespaces" }, description = "If root contains more than one tablespace, specify here name of the tablespaces that should be deployed. A folder with the partition-map file and the store folder should be into the path <root>/<tablespace>. Several can be provided.")
-	private List<String> tablespaces = new ArrayList<String>();
-	
-	@Parameter(names = { "-c", "--config-file" }, description = "Use the given filename as deployment spec. If present, <root> <tablespace> and <repfactor> options are ignored. In local filesystem.")
-	private String configFile = null;
-	
-	private Configuration conf;
+  @Parameter(names = {"-tn", "--tablespacename"}, description = "If root is a generated tablespace - for example, a bucket in S3 - this parameter provides its name. Either use this or tablespaces parameter if root contains multiple tablespaces.")
+  private String tablespaceName = null;
 
-	@Override
-	public Configuration getConf() {
-		return conf;
-	}
+  @Parameter(names = {"-ts", "--tablespaces"}, description = "If root contains more than one tablespace, specify here name of the tablespaces that should be deployed. A folder with the partition-map file and the store folder should be into the path <root>/<tablespace>. Several can be provided.")
+  private List<String> tablespaces = new ArrayList<String>();
 
-	@Override
-	public void setConf(Configuration conf) {
-		this.conf = conf;
-	}
+  @Parameter(names = {"-c", "--config-file"}, description = "Use the given filename as deployment spec. If present, <root> <tablespace> <repfactor> and <init-statements> options are ignored. In local filesystem.")
+  private String configFile = null;
 
-	@Override
-	public int run(String[] args) throws Exception {
-		JCommander jComm = new JCommander(this);
-		jComm.setProgramName("Tablespaces Deployer");
-		try {
-			jComm.parse(args);
-		} catch (ParameterException e) {
-			System.out.println(e.getMessage());
-			jComm.usage();
-			return -1;
-		} catch (Throwable t) {
-			t.printStackTrace();
-			jComm.usage();
-			return -1;
-		}
-		
-		StoreDeployerTool deployer = new StoreDeployerTool(qnode, getConf());		
-		
-		ArrayList<TablespaceDepSpec> deployments = new ArrayList<TablespaceDepSpec>();
-		if (configFile != null) {
-			deployments = JSONSerDe.deSer(Files.toString(new File(configFile), Charset.forName("UTF-8")), new TypeReference<ArrayList<TablespaceDepSpec>>() {});
-		} else {
-			Path rootPath = new Path(root);
-			if(tablespaceName == null && tablespaces.size() == 0) {
-				System.err.println("Tablespace name for root folder or tablespaces contained in them is lacking. Either use tablespacename or tablespaces option.");
-				jComm.usage();
-				return -1;				
-			}
-			if(tablespaceName != null && tablespaces.size() > 0) {
-				System.err.println("Can't use tablespacename and tablespaces at the same time. Root is to be either a generated tablespace or a folder with multiple generated tablespaces.");
-				jComm.usage();
-				return -1;
-			}
-			if(tablespaceName != null) {
-				deployments.add(new TablespaceDepSpec(tablespaceName, rootPath.toString(), replicationFactor));
-			}
-			for (String tb : tablespaces) {
-				Path tablespacePath = new Path(rootPath, tb);
-				deployments.add(new TablespaceDepSpec(tb, tablespacePath.toString(), replicationFactor));				
-			}
-		}
-		
-		// Checking for file existence
-		for (TablespaceDepSpec spec : deployments) {
-			Path tablespacePath = new Path(spec.getSourcePath());
-			FileSystem fs = tablespacePath.getFileSystem(getConf());
-			if (!fs.exists(tablespacePath)) {
-				System.out.println("ERROR: Path [" + tablespacePath.makeQualified(fs) + "] not found.");
-				return 1;
-			}
-		}
+  @Parameter(names = {"-is", "--init-statements"}, description = "Statements that will be executed just before starting to serve a partition. Ideal place to put PRAGMA statements, like cache-size")
+  private List<String> initStatements = new ArrayList<String>();
 
-		deployer.deploy(deployments);
-		return 0;
-	}
+  private Configuration conf;
 
-	public static void main(String[] args) throws Exception {
-		ToolRunner.run(new DeployerCMD(), args);
-	}
+  @Override
+  public Configuration getConf() {
+    return conf;
+  }
+
+  @Override
+  public void setConf(Configuration conf) {
+    this.conf = conf;
+  }
+
+  @Override
+  public int run(String[] args) throws Exception {
+    JCommander jComm = new JCommander(this);
+    jComm.setProgramName("Tablespaces Deployer");
+    try {
+      jComm.parse(args);
+    } catch (ParameterException e) {
+      System.out.println(e.getMessage());
+      jComm.usage();
+      return -1;
+    } catch (Throwable t) {
+      t.printStackTrace();
+      jComm.usage();
+      return -1;
+    }
+
+    StoreDeployerTool deployer = new StoreDeployerTool(qnode, getConf());
+
+    ArrayList<TablespaceDepSpec> deployments = new ArrayList<TablespaceDepSpec>();
+    if (configFile != null) {
+      deployments = JSONSerDe.deSer(Files.toString(new File(configFile), Charset.forName("UTF-8")), new TypeReference<ArrayList<TablespaceDepSpec>>() {
+      });
+    } else {
+      Path rootPath = new Path(root);
+      if (tablespaceName == null && tablespaces.size() == 0) {
+        System.err.println("Tablespace name for root folder or tablespaces contained in them is lacking. Either use tablespacename or tablespaces option.");
+        jComm.usage();
+        return -1;
+      }
+      if (tablespaceName != null && tablespaces.size() > 0) {
+        System.err.println("Can't use tablespacename and tablespaces at the same time. Root is to be either a generated tablespace or a folder with multiple generated tablespaces.");
+        jComm.usage();
+        return -1;
+      }
+      if (tablespaceName != null) {
+        deployments.add(new TablespaceDepSpec(tablespaceName, rootPath.toString(), replicationFactor, initStatements));
+      }
+      for (String tb : tablespaces) {
+        Path tablespacePath = new Path(rootPath, tb);
+        deployments.add(new TablespaceDepSpec(tb, tablespacePath.toString(), replicationFactor, initStatements));
+      }
+    }
+
+    // Checking for file existence
+    for (TablespaceDepSpec spec : deployments) {
+      Path tablespacePath = new Path(spec.getSourcePath());
+      FileSystem fs = tablespacePath.getFileSystem(getConf());
+      if (!fs.exists(tablespacePath)) {
+        System.out.println("ERROR: Path [" + tablespacePath.makeQualified(fs) + "] not found.");
+        return 1;
+      }
+    }
+
+    deployer.deploy(deployments);
+    return 0;
+  }
+
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new DeployerCMD(), args);
+  }
 }
