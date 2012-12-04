419e63ce004133aded115acb07be3cd3a5d01331
Iván de Prado <ivan.prado@gmail.com>
2012-12-04 18:26:26 +0100

    Support in CMD API for initStatements on serving. Solved a bug.

--- splout-hadoop/src/main/java/com/splout/db/hadoop/StoreDeployerTool.java
+++ splout-hadoop/src/main/java/com/splout/db/hadoop/StoreDeployerTool.java
@@ -20,17 +20,6 @@ package com.splout.db.hadoop;
  * #L%
  */
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-
 import com.datasalt.pangool.utils.HadoopUtils;
 import com.splout.db.common.JSONSerDe;
 import com.splout.db.common.JSONSerDe.JSONSerDeException;
@@ -39,86 +28,102 @@ import com.splout.db.common.ReplicationMap;
 import com.splout.db.common.SploutClient;
 import com.splout.db.qnode.beans.DeployInfo;
 import com.splout.db.qnode.beans.DeployRequest;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * A generic class for deploying an already generated store by {@link TablespaceGenerator}.
  */
 public class StoreDeployerTool {
 
-	private final static Log log = LogFactory.getLog(StoreDeployerTool.class);
+  private final static Log log = LogFactory.getLog(StoreDeployerTool.class);
 
-	private String qnode;
-	private Configuration conf;
+  private String qnode;
+  private Configuration conf;
 
-	public StoreDeployerTool(String qnode, Configuration conf) {
-		this.qnode = qnode;
-		this.conf = conf;
-	}
-	
-	/**
-	 * Deploy already generated tablespaces at a time. 
-	 */
-	@SuppressWarnings("unchecked")
+  public StoreDeployerTool(String qnode, Configuration conf) {
+    this.qnode = qnode;
+    this.conf = conf;
+  }
+
+  /**
+   * Deploy already generated tablespaces at a time.
+   */
+  @SuppressWarnings("unchecked")
   public void deploy(Collection<TablespaceDepSpec> deployments) throws JSONSerDeException, IOException {
-		
-		// We now query for the alive DNodes and build deployRequests accordingly
-		DeployRequest[] deployRequests = new DeployRequest[deployments.size()];
 
-		log.info("Querying Splout QNode for list of DNodes...");
-		SploutClient client = new SploutClient(qnode);
-		List<String> dnodes = client.dNodeList();		
-		
-		int tIndex = 0;
-		for(TablespaceDepSpec tablespace : deployments) {
-			Path tablespaceOut = new Path(tablespace.getSourcePath());
+    // We now query for the alive DNodes and build deployRequests accordingly
+    DeployRequest[] deployRequests = new DeployRequest[deployments.size()];
 
-			// Define a DeployRequest for this Tablespace
-			deployRequests[tIndex] = new DeployRequest();
-						
-			// Splout only accepts absolute URIs
-			FileSystem sourceFs = tablespaceOut.getFileSystem(conf);
-			if(!sourceFs.exists(tablespaceOut)) {
-				throw new IllegalArgumentException("Folder doesn't exist: " + tablespaceOut);
-			}
-			Path absoluteOutPath = tablespaceOut.makeQualified(sourceFs);
-			
-			Path partitionMapPath = new Path(tablespaceOut, TablespaceGenerator.OUT_PARTITION_MAP);
-			if(!sourceFs.exists(partitionMapPath)) {
-				throw new IllegalArgumentException("Invalid tablespace folder: " + tablespaceOut + " doesn't contain a partition-map file.");
-			}
-			
-			// Load the partition map
-			PartitionMap partitionMap = JSONSerDe.deSer(
-			    HadoopUtils.fileToString(sourceFs, partitionMapPath), PartitionMap.class);
-			    			
-			// Load the init statements, if they exist
-			Path initStatementsPath = new Path(tablespaceOut, TablespaceGenerator.OUT_INIT_STATEMENTS);
-			if(sourceFs.exists(initStatementsPath)) {
-				List<String> initStatements = JSONSerDe.deSer(HadoopUtils.fileToString(sourceFs, initStatementsPath), ArrayList.class);
-				deployRequests[tIndex].setInitStatements(initStatements);
-			}
-			
-			deployRequests[tIndex].setTablespace(tablespace.getTablespace());
-			deployRequests[tIndex].setData_uri(new Path(absoluteOutPath, "store").toUri().toString());
-			deployRequests[tIndex].setPartitionMap(partitionMap.getPartitionEntries());
-			
-			// If rep>dnodes, imposible to reach this level of replication
-			int repFactor = tablespace.getReplication();
-			if (dnodes.size()<repFactor) {
-				System.out.println("WARNING: Replication factor " + repFactor +" for tablespace " + tablespace.getTablespace() + " is bigger than the number of serving DNodes. Adjusting replication factor to "+dnodes.size());
-				repFactor = dnodes.size();
-			} 
+    log.info("Querying Splout QNode for list of DNodes...");
+    SploutClient client = new SploutClient(qnode);
+    List<String> dnodes = client.dNodeList();
 
-			deployRequests[tIndex].setReplicationMap(ReplicationMap.roundRobinMap(partitionMap.getPartitionEntries().size(), repFactor,
-			    dnodes.toArray(new String[0])).getReplicationEntries());
-			
-			tIndex++;
-		}
-		
-		// Finally we send the deploy request
-		DeployInfo dInfo = client.deploy(deployRequests);
+    int tIndex = 0;
+    for (TablespaceDepSpec tablespace : deployments) {
+      Path tablespaceOut = new Path(tablespace.getSourcePath());
 
-		log.info("Deploy request of [" + deployments.size() + "] tablespaces performed. Deploy on [" + qnode + "] with version [" + dInfo.getVersion() + "] in progress.");
-	}
-	
+      // Define a DeployRequest for this Tablespace
+      deployRequests[tIndex] = new DeployRequest();
+
+      // Splout only accepts absolute URIs
+      FileSystem sourceFs = tablespaceOut.getFileSystem(conf);
+      if (!sourceFs.exists(tablespaceOut)) {
+        throw new IllegalArgumentException("Folder doesn't exist: " + tablespaceOut);
+      }
+      Path absoluteOutPath = tablespaceOut.makeQualified(sourceFs);
+
+      Path partitionMapPath = new Path(tablespaceOut, TablespaceGenerator.OUT_PARTITION_MAP);
+      if (!sourceFs.exists(partitionMapPath)) {
+        throw new IllegalArgumentException("Invalid tablespace folder: " + tablespaceOut + " doesn't contain a partition-map file.");
+      }
+
+      // Load the partition map
+      PartitionMap partitionMap = JSONSerDe.deSer(
+          HadoopUtils.fileToString(sourceFs, partitionMapPath), PartitionMap.class);
+
+      // Load the init statements, if they exist
+      ArrayList<String> initStatements = new ArrayList<String>();
+      Path initStatementsPath = new Path(tablespaceOut, TablespaceGenerator.OUT_INIT_STATEMENTS);
+      if (sourceFs.exists(initStatementsPath)) {
+        initStatements.addAll(JSONSerDe.deSer(HadoopUtils.fileToString(sourceFs, initStatementsPath), ArrayList.class));
+      }
+      // Add the other initStatements coming in the deploy request
+      if (tablespace.getInitStatements() != null) {
+        initStatements.addAll(tablespace.getInitStatements());
+      }
+      // Finally set
+      deployRequests[tIndex].setInitStatements(new ArrayList<String>());
+
+      deployRequests[tIndex].setTablespace(tablespace.getTablespace());
+      deployRequests[tIndex].setData_uri(new Path(absoluteOutPath, "store").toUri().toString());
+      deployRequests[tIndex].setPartitionMap(partitionMap.getPartitionEntries());
+
+      // If rep>dnodes, imposible to reach this level of replication
+      int repFactor = tablespace.getReplication();
+      if (dnodes.size() < repFactor) {
+        System.out.println("WARNING: Replication factor " + repFactor + " for tablespace " + tablespace.getTablespace() + " is bigger than the number of serving DNodes. Adjusting replication factor to " + dnodes.size());
+        repFactor = dnodes.size();
+      }
+
+      deployRequests[tIndex].setReplicationMap(ReplicationMap.roundRobinMap(partitionMap.getPartitionEntries().size(), repFactor,
+          dnodes.toArray(new String[0])).getReplicationEntries());
+
+      tIndex++;
+    }
+
+    // Finally we send the deploy request
+    DeployInfo dInfo = client.deploy(deployRequests);
+
+    log.info("Deploy request of [" + deployments.size() + "] tablespaces performed. Deploy on [" + qnode + "] with version [" + dInfo.getVersion() + "] in progress.");
+  }
+
 }
